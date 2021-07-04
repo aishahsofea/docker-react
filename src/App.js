7109c99366d2926812a043957f010f253c5d2eed
aishahsofea <aishah.s.hanifa@gmail.com>
2021-07-05 00:22:35 +0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Test changes on feature
         </a>
       </header>
     </div>
